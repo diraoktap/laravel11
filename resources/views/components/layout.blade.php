16815b96eb94a1ee41b20db6e2afea397edfc5c1
diraoktap <diraoktap.me@gmail.com>
2024-09-11 11:24:33 +0700

    merubah font

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,8 +10,9 @@
     {{-- Javascript --}}
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     {{-- fonts --}}
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-
+    <link
+        href="https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
 
     <title>Home</title>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ['Poppins'],
       },
     },
   },
