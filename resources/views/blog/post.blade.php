90e41f2fed7627b8b8cd669ec5a14df1bbb73027
diraoktap <diraoktap.me@gmail.com>
2024-09-11 11:24:56 +0700

    menambah halaman post + web route

---
+++ resources/views/blog/post.blade.php
@@ -0,0 +1,65 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <article class="flex flex-col items-start justify-center space-y-6">
+        <div class="flex items-center gap-x-4 text-xs">
+            <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+            <p class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600">
+                {{ $post['category'] }}
+            </p>
+        </div>
+        <h1 class="mt-3 text-4xl text-center font-semibold leading-6 text-gray-900">
+            {{ $post['title'] }}
+        </h1>
+        <p class="mt-5 text-sm leading-6 text-gray-600">
+            {{ $post['content'] }}
+        </p>
+
+        <div class="relative mt-8 flex items-center gap-x-4">
+            <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt="" class="h-10 w-10 rounded-full bg-gray-50">
+            <div class="text-sm leading-6">
+                <p class="font-semibold text-gray-900">
+                    <a href="#">
+                        <span class="absolute inset-0"></span>
+                        {{ $post['author'] }}
+                    </a>
+                </p>
+                <p class="text-gray-600">{{ $post['role'] }}</p>
+            </div>
+        </div>
+
+        <a href="/posts"
+            class="text-xs font-semibold text-gray-600 hover:text-gray-900 transition-all duration-300">&laquo; Back to
+            blog</a>
+    </article>
+
+    {{-- <article class="flex flex-col items-start justify-between">
+        <div class="flex items-center gap-x-4 text-xs">
+            <time datetime="2020-03-16" class="text-gray-500">Mar 16, 2020</time>
+            <a href="/posts/{{ $post['id'] }}"
+                class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">{{ $post['category'] }}</a>
+        </div>
+        <div class="relative flex flex-col content-center">
+            <h3 class="mt-3 text-lg  font-semibold leading-6 text-gray-900">
+                {{ $post['title'] }}
+            </h3>
+            <p class="mt-5 text-sm leading-6 text-gray-600">
+                {{ $post['content'] }}
+            </p>
+        </div>
+        <div class="relative mt-8 flex items-center gap-x-4">
+            <img src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                alt="" class="h-10 w-10 rounded-full bg-gray-50">
+            <div class="text-sm leading-6">
+                <p class="font-semibold text-gray-900">
+                    <a href="#">
+                        <span class="absolute inset-0"></span>
+                        {{ $post['author'] }}
+                    </a>
+                </p>
+                <p class="text-gray-600">{{ $post['role'] }}</p>
+            </div>
+        </div>
+    </article> --}}
+
+</x-layout>
